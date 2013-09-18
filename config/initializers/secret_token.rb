285b2b1e8dce9e642e7cad7280fdf21a1222b9d5
Scott Norvell <scottlnorvell@gmail.com>
2013-09-18 17:08:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeScottRails::Application.config.secret_token = 'b1a8545d302d4aed440ae73f267421b840e57d0dbfdf2faa5358cd6cab3c01d9d35d885f35fc69b3f0b1e5825fa61eb9569f364f88256a787f2465290f530c46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BeScottRails::Application.config.session_store :cookie_store, key: '_be_scott_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BeScottRails::Application.config.session_store :active_record_store
